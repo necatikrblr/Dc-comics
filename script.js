8c67ce8414d6f54141bd2ef82e2a67f6f7b057b4
Necati <necati.karabiberler35@gmail.com>
2023-04-28 04:05:34 +0300

    Dosyalar yüklendi

---
+++ script.js
@@ -0,0 +1,6 @@
+// let myCarousel = document.querySelector('#carouselExampleIndicators')
+// let carousel = new bootstrap.Carousel(myCarousel, {
+//   interval: 2000,
+//   wrap: true,
+//   Touch:true,
+// })
